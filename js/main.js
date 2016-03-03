453be8827b0e2a9b83902a06d3c6d47f3f94c7c1
mhoaglund <maxwell.hoaglund@gmail.com>
2016-03-02 20:23:48 -0600

    dendrite refinement and corrected layer matching

--- js/main.js
+++ js/main.js
@@ -1,32 +1,145 @@
+//helpers
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
+//http://stackoverflow.com/questions/8188548/splitting-a-js-array-into-n-arrays @pimvdb
+function splitUp(arr, n) {
+    var rest = arr.length % n, // how much to divide
+        restUsed = rest, // to keep track of the division over the elements
+        partLength = Math.floor(arr.length / n),
+        result = [];
+
+    for(var i = 0; i < arr.length; i += partLength) {
+        var end = partLength + i,
+            add = false;
+
+        if(rest !== 0 && restUsed) { // should add one element for the division
+            end++;
+            restUsed--; // we've used one division element now
+            add = true;
+        }
+
+        result.push(arr.slice(i, end)); // part of the array
+
+        if(add) {
+            i++; // also increment i in the case we added an extra element for division
+        }
+    }
+
+    return result;
+}
+//end helpers
+
 var nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1', fixed: {
-      x:true,
-      y:true
-    } },
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'}
+    {id: 1, label: '', fixed: {x:true,y:true}},
+    {id: 2, label: '', fixed: {x:true,y:true}}
 ]);
+
+var layers = [
+    {layer:0, indices:[1]},
+    {layer:1, indices:[2]}
+];
 
 // create an array with edges
 //can spoof the hubsize organizer by adding bogus self-connections
 var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
+    {from: 1, to: 2, width: 4}
 ]);
 
-// create a network
 var container = document.getElementById('visbody');
-var i = 6;
-var j = 5;
-var CurrentLayer = 0;
+var CurrentLayer = 1;
+var CurrentIndexStop = 2;
+var BroadestLayer = 6;
 
-function addTreeLayer(layerno){
+function Init(){
+    container = document.getElementById('visbody');
+    i = 6;
+    j = 5;
+    CurrentLayer = 1;
+    CurrentIndexStop = 2;
+    BroadestLayer = 6;
+    nodes = new vis.DataSet([
+        {id: 1, label: '', fixed: {x:true,y:true}},
+        {id: 2, label: '', fixed: {x:true,y:true}}
+    ]);
+    layers = [
+        {layer:0, indices:[1]},
+        {layer:1, indices:[2]}
+    ];
+    edges = new vis.DataSet([
+        {from: 1, to: 2, width: 4}
+    ]);
+}
+
+function addTreeLayer(){
+    var newNodeIndices = [];
+    if(CurrentLayer == 1){
+        addDummyEdges((BroadestLayer * 2), 1);
+        nodes.add({id: 3, label: ''});
+        newNodeIndices.push(3);
+        nodes.add({id: 4, label: ''});
+        newNodeIndices.push(4);
+        nodes.add({id: 5, label: ''});
+        newNodeIndices.push(5);
+    }
+    //TODO: incrementality
+    else{
+        if(CurrentLayer == 2){
+        addDummyEdges(BroadestLayer, 1);
+        addDummyEdges(BroadestLayer, 2);
+        }
+        for (i = 1; i <= BroadestLayer; i++) { 
+            var myId = CurrentIndexStop+i;
+            nodes.add({id: myId, label: ''});
+            newNodeIndices.push(myId);
+        }
+    }
+    CurrentIndexStop = getMaxOfArray(newNodeIndices);
+    layers.push({layer: CurrentLayer + 1, indices: newNodeIndices});
+    establishEdges(newNodeIndices);
+    lockLastLayer();
+    // reconcileSorting();
+    CurrentLayer++;
+}
+
+function establishEdges(newNodes){
+    //var splitFactor = (CurrentLayer > 1) ? CurrentLayer/2 : CurrentLayer;
+    var linkTo;
+    var currWidth = 1;
+    if(CurrentLayer == 1){
+        linkTo = layers.filter(function(obj){
+            return obj.layer === CurrentLayer;
+        })[0];
+        currWidth = 2;
+    }else{
+        linkTo = layers.filter(function(obj){
+            return obj.layer === CurrentLayer-1;
+        })[0];
+    }
+    
+    if(linkTo == null) return;
+
+    for (var i = 0; i < newNodes.length; i++) {
+        var linkTarget = linkTo.indices[Math.floor(Math.random()*linkTo.indices.length)];
+        edges.add({from: newNodes[i], to: linkTarget, width: currWidth});
+    }
+}
+
+function lockLastLayer(){
     
 }
+//TODO: flesh this out so when layers are added, 'weight' is added to existing layers to preserve sorting heirarchy
+// function reconcileSorting(){
+//     for(var thislayer in layers){
+//         if(thislayer.layer < CurrentLayer){
+//             thislayer.indices.forEach(
+//                 //addDummyEdge(value);
+                
+//             );
+//         }
+//     }
+// }
 
 function addNode() {
     var newId = i;
@@ -36,22 +149,28 @@ function addNode() {
     j++;
 }
     
-    
-function addDummyEdge(t) {
+function addDummyEdge(target) {
     var d = ({
-    from: t, 
-    to: t, 
+    from: target, 
+    to: target, 
     width: 0, 
     color: '#222',
-    shadow:{
-        enabled: false,
-        color: 'rgba(0,0,0,1)',
-        size:15,
-        x:5,
-        y:5
-        }
+    shadow:{enabled: false}
     });
     edges.add(d);
+}
+
+function addDummyEdges(amount, target) {
+    for(i = 0; i<amount; i++){
+        var d = ({
+            from: target, 
+            to: target, 
+            width: 0, 
+            color: '#222',
+            shadow:{enabled: false}
+        });
+        edges.add(d);
+    }
 }
 
 // provide the data in the vis format
@@ -170,7 +289,7 @@ var options = {
         onlyDynamicEdges: false,
         fit: true
         },
-        timestep: 0.5,
+        timestep: 0.9,
         adaptiveTimestep: true
     }
 }
@@ -185,5 +304,5 @@ network.on("stabilized", function (params) {
 var intervalID = window.setInterval(myCallback, 4500);
 
 function myCallback() {
-  addNode();
+  addTreeLayer();
 }
